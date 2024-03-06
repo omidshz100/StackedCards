d255125ffccc4a85c788cfefbd7304887d0f2965
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-03-06 13:58:00 +0100

    Initial Commit

---
+++ StackedCards/StackedCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackedCardsApp.swift
+//  StackedCards
+//
+//  Created by Omid Shojaeian Zanjani on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct StackedCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
